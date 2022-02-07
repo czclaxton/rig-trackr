6abf7a27511760cc2c5f728d488fecc80717e17b
Connor Claxton <cclaxton@intrepidcs.com>
2022-02-07 18:35:43 -0500

    added routing and some more basic setup and deps

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from '../pages/Home'
 
-function App() {
-  return <div className='App'>Hello World!</div>
+const App = () => {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route index element={<Home />} />
+        </Routes>
+      </Router>
+    </>
+  )
 }
 
 export default App
